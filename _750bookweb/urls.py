7281ecf26cb9b0cdf2bf4f12943e7b4fe6f2c2c6
Kevin Riggle <kevinr@free-dissociation.com>
2011-11-09 13:11:41 -0500

    redirect root to beginning of workflow

--- _750bookweb/urls.py
+++ _750bookweb/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
-from django.views.generic.simple import direct_to_template
+from django.views.generic.simple import redirect_to
 
 from django.contrib import admin
 admin.autodiscover()
@@ -10,8 +10,8 @@ handler500 = "pinax.views.server_error"
 
 
 urlpatterns = patterns("",
-    url(r"^$", direct_to_template, {
-        "template": "homepage.html",
+    url(r"^$", redirect_to, {
+        "url": "/bookmaker/create/",
     }, name="home"),
     url(r"^admin/", include(admin.site.urls)),
     url(r"^bookmaker/", include('bookmaker.urls')),
