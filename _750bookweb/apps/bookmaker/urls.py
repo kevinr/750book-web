a1b28f139738a7d4b464975bd23798cd620bf52b
Kevin Riggle <kevinr@free-dissociation.com>
2011-11-09 21:23:55 -0500

    redirect bookmaker -> bookmaker/create

--- _750bookweb/apps/bookmaker/urls.py
+++ _750bookweb/apps/bookmaker/urls.py
@@ -1,9 +1,10 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
-from django.views.generic.simple import direct_to_template
+from django.views.generic.simple import redirect_to
 
 
 urlpatterns = patterns("bookmaker.views",
+    url(r"^$", redirect_to, { 'url': 'create/', }, name="bookmaker"),
     url(r"^create/$", 'create_update_submission', name="create"),
     url(r"^(?P<nonce>[\da-fA-F-]+)/$", 'review', name="review"),
     url(r"^(?P<nonce>[\da-fA-F-]+)/update/$", 'create_update_submission', name="update"),
