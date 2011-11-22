b4773031cfeb565c5e827c7716b43df34eee0f3d
Kevin Riggle <kevinr@free-dissociation.com>
2011-11-22 18:20:53 -0500

    make environment relocatable

--- 750book-web-env/bin/django-admin.py
+++ 750book-web-env/bin/django-admin.py
@@ -1,4 +1,5 @@
-#!/home/kevinr/src/750book-web-project/750book-web-env/bin/python
+#!/usr/bin/env python2.7
+import os; activate_this=os.path.join(os.path.dirname(__file__), 'activate_this.py'); execfile(activate_this, dict(__file__=activate_this)); del os, activate_this
 from django.core import management
 
 if __name__ == "__main__":
